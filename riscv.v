6a86484935d30f35bd4b8f2baa382b74c6ae1005
Jeff Larson <thejefflarson@gmail.com>
2020-01-17 15:46:57 -0800

    more

--- riscv.v
+++ riscv.v
@@ -16,23 +16,61 @@ module riscv (
   input         bready, // status is ready
   input         bresp, // status of our write request
 
-  output        arvalid, // we read the address
-  input         arready, // address is ready for reading
-  input  [31:0] araddress, // address to read
+  output        arvalid, // we put something in the read addreas
+  input         arready, // they are reading thej value
+  output [31:0] araddress, // address to read
   output [2:0]  arprot, // permissions
 
-  output        rvalid, // we read the value
-  input         rready, // value is ready
+  input         rvalid, // Data is valid and can be read by us
+  output        rready, // we are ready to read
   input  [31:0] rdata, // value to read
   input         rresp // status of our read request
 );
-  reg [31:0] regs[0:31];
   assign wrstrb = 4'b1;
+  reg [31:0] regs[0:31];
+  reg [31:0] pc;
+  reg [31:0] instr;
+
+  reg 	     trap; // ruroh
+  reg 	     execute; // we can execute
+  reg [31:0] next_pc;
+  reg [31:0] next_instr;
+
+  integer i;
 
   always @(posedge clk) begin
-     if (reset) begin
-        for (integer i = 0; i < 32; i = i + 1)
-          regs[i] <= 0;
+     if (!reset) begin
+        for (i = 0; i < 32; i = i + 1) begin
+          regs[i] <= 31'b0;
+	  pc <= 31'b0;
+	end
      end
   end
+
+  // instruction fetcher
+  always @(posedge clk) begin
+    if (!reset) begin
+      arvalid <= 0; // we haven't put anything in the address
+      rready <= 1; // we are ready to read
+    end else begin
+
+    end
+  end
+
+  // opcode decoder
+  always @(posedge clk) begin
+    if(!reset) begin
+
+    end
+  end
+
+  // instruction decoder
+  always @(posedge clk) begin
+    if(!reset) begin
+
+    end
+  end
+
+  // exectution
+
 endmodule
