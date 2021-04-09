4f0a13f58e43ccf8e43deb49199ce466bef6e13a
Jeff Larson <thejefflarson@gmail.com>
2021-04-08 23:25:54 -0700

    make it work like a single linear memory

--- rtl/memory.v
+++ rtl/memory.v
@@ -17,17 +17,19 @@ module memory #(
   logic [31:0] instr[0:INSTR-1];
   initial $readmemb(ROM, instr);
 
+  logic [31:0] ram_addr;
+  assign ram_addr = mem_addr - (INSTR << 2);
   logic [31:0] ram[0:RAM-1];
   always_ff @(posedge clk) begin
     if (!mem_instr) begin
       mem_valid <= 0;
       if (mem_ready && !mem_valid) begin
-        if (mem_addr < 4*RAM) begin
-          mem_rdata <= ram[mem_addr >> 2];
-          if(mem_wstrb[0]) ram[mem_addr >> 2][7:0] <= mem_wdata[7:0];
-          if(mem_wstrb[1]) ram[mem_addr >> 2][15:8] <= mem_wdata[15:8];
-          if(mem_wstrb[2]) ram[mem_addr >> 2][23:16] <= mem_wdata[23:16];
-          if(mem_wstrb[3]) ram[mem_addr >> 2][31:24] <= mem_wdata[31:24];
+        if (ram_addr < 4*RAM) begin
+          mem_rdata <= ram[ram_addr >> 2];
+          if(mem_wstrb[0]) ram[ram_addr >> 2][7:0] <= mem_wdata[7:0];
+          if(mem_wstrb[1]) ram[ram_addr >> 2][15:8] <= mem_wdata[15:8];
+          if(mem_wstrb[2]) ram[ram_addr >> 2][23:16] <= mem_wdata[23:16];
+          if(mem_wstrb[3]) ram[ram_addr >> 2][31:24] <= mem_wdata[31:24];
           mem_valid <= 1;
         end
       end
